28106bc6dfc3cce412ff6c08d452023769de735c
花落永恒 <zjh@hualaoyongheng.local>
2017-07-20 11:22:17 +0800

    change

---
+++ ProductBaseClasses/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProductBaseClasses
+//
+//  Created by 花落永恒 on 2017/7/17.
+//  Copyright © 2017年 划落永恒. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ProductBaseClasses/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ProductBaseClasses
+//
+//  Created by 花落永恒 on 2017/7/17.
+//  Copyright © 2017年 划落永恒. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
